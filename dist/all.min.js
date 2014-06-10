13fa7d91902e1b72a0c91d952e8512c576f249e2
gomako <bgharvey@gmail.com>
2014-06-10 17:19:03 +0100

    Up and down, up and down!

--- dist/all.min.js
+++ dist/all.min.js
@@ -3,43 +3,7 @@ var elem = document.getElementById('bork');
 var params = { width: document.body.clientWidth, height: document.body.clientHeight };
 var two = new Two(params).appendTo(elem);
 
-
-// From clrs.cc
-var colors = [
-	"#7FDBFF",
-	"#0074D9",
-	"#01FF70",
-	"#001F3F",
-	"#39CCCC",
-	"#3D9970",
-	"#2ECC40",
-	"#FF4136",
-	"#85144B",
-	"#FF851B",
-	"#B10DC9",
-	"#FFDC00",
-	"#F012BE",
-	"#aaa",
-	"#fff",
-	"#111",
-	"#ddd"
-];
-
-// Load Skullz
-var skullz = [];
-for (var i = 0; i < 100; i++)
-{
-	skullz.push(two.interpret(document.getElementById('skullySkull')));
-}
-
-// Loop through skulls and fuck about with them
-for (var i = skullz.length - 1; i >= 0; i--)
-{
-	skullz[i].translation.x = Math.random()*params.width - skullz[i].getBoundingClientRect().width;
-	skullz[i].translation.y = Math.random()*params.height - skullz[i].getBoundingClientRect().height;
-	skullz[i].fill = colors[Math.floor(Math.random() * colors.length)];
-	skullz[i].opacity = 0.5;
-}
-
-// Render
+var skulleh = two.interpret(document.getElementById('skullySkull'));
+skulleh.center();
+skulleh.translation.set(two.width / 2, two.height / 2);
 two.update();
--- js/main.js
+++ js/main.js
@@ -1,45 +1,35 @@
 // Make an instance of two and place it on the page.
 var elem = document.getElementById('bork');
-var params = { width: document.body.clientWidth, height: document.body.clientHeight };
+var params = { width: document.body.clientWidth, height: document.body.clientHeight-10 };
 var two = new Two(params).appendTo(elem);
 
+var flames = two.interpret(document.getElementById('flames'));
+var face = two.interpret(document.getElementById('face'));
 
-// From clrs.cc
-var colors = [
-	"#7FDBFF",
-	"#0074D9",
-	"#01FF70",
-	"#001F3F",
-	"#39CCCC",
-	"#3D9970",
-	"#2ECC40",
-	"#FF4136",
-	"#85144B",
-	"#FF851B",
-	"#B10DC9",
-	"#FFDC00",
-	"#F012BE",
-	"#aaa",
-	"#fff",
-	"#111",
-	"#ddd"
-];
+var angle = 0;
 
-// Load Skullz
-var skullz = [];
-for (var i = 0; i < 100; i++)
-{
-	skullz.push(two.interpret(document.getElementById('skullySkull')));
-}
+// Do some translation voodoo to get the rotation point right
+var f = face.getBoundingClientRect();
+face.translation.set( -(f.left + f.width/2), -(f.top + f.height/2)+2 );
+flames.translation.set( -(f.left + f.width/2), -(f.top + f.height/2)+5 );
 
-// Loop through skulls and fuck about with them
-for (var i = skullz.length - 1; i >= 0; i--)
-{
-	skullz[i].translation.x = Math.random()*params.width - skullz[i].getBoundingClientRect().width;
-	skullz[i].translation.y = Math.random()*params.height - skullz[i].getBoundingClientRect().height;
-	skullz[i].fill = colors[Math.floor(Math.random() * colors.length)];
-	skullz[i].opacity = 0.5;
-}
+var fire = two.makeGroup(flames);
+var skulleh = two.makeGroup(fire, face);
 
-// Render
-two.update();
+skulleh.center();
+skulleh.translation.set(two.width / 2, two.height / 2);
+
+two.bind('update', function(frameCount) {
+  
+	if(angle > Math.PI*2)
+	{
+		angle = 0;
+	}
+	fire.rotation         = (Math.sin(angle)*0.3)+0.35;
+	face.translation.y    -= Math.sin(angle)*0.1;
+	skulleh.translation.y -= Math.sin(angle)*5;
+	skulleh.translation.x -= Math.sin(angle);
+
+  angle += 0.09;
+
+}).play();
